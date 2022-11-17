c048f67bb4a847e675ab09cf4bedd7c1a541a2e3
Daniel Rebolledo <daniel.rebolledo@tsoftlatam.com>
2022-11-17 16:15:49 -0300

    Prueba numero 2

--- SpecflowTEst/Hooks/HooksSel.cs
+++ SpecflowTEst/Hooks/HooksSel.cs
@@ -3,7 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
-using SpecflowTEst.Drivers;
+
 using TechTalk.SpecFlow;
 
 namespace SpecflowTEst.Hooks
--- SpecflowTEst/StepDefinitions/GSearchSteps.cs
+++ SpecflowTEst/StepDefinitions/GSearchSteps.cs
@@ -1,11 +1,6 @@
-﻿using NUnit.Framework;
+﻿
 using OpenQA.Selenium;
-using SpecflowTEst.Drivers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace SpecflowTEst.StepDefinitions
 {
